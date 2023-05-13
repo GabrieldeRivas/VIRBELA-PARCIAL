665562293345b3d0e32d5c8dbb47413434b59fd7
Gabriel de Rivas <gabriel.derivas@estudiantes.unahur.edu.ar>
2023-05-13 20:14:50 -0300

    pag completa

--- main.js
+++ main.js
@@ -15,3 +15,41 @@ let linkedinIcon = document.querySelector("#icon-linkedin");
 linkedinIcon.addEventListener("click", () => {
     location.href ='https://www.linkedin.com/company/virbela/';
 })
+
+let facebookIconCel = document.querySelector("#icon-facebook-cel");
+
+facebookIconCel.addEventListener("click", () => {
+    location.href ='https://www.facebook.com/Virbela/';
+})
+
+let twitterIconCel = document.querySelector("#icon-twitter-cel");
+
+twitterIconCel.addEventListener("click", () => {
+    location.href ='https://twitter.com/VirbelaHQ';
+})
+
+let linkedinIconCel = document.querySelector("#icon-linkedin-cel");
+
+linkedinIconCel.addEventListener("click", () => {
+    location.href ='https://www.linkedin.com/company/virbela/';
+})
+
+let registerButton = document.querySelector("#register-button");
+
+registerButton.addEventListener("click", () => {
+    location.href ='register.html';
+})
+
+// DARK-MODE TOGGLE
+
+const btnSwitch = document.querySelector(".dark-toggle");
+
+btnSwitch.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+})
+
+const btnSwitchCel = document.querySelector("#dark-toggle-cel");
+
+btnSwitchCel.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+})
