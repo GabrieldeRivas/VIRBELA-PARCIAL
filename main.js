7f015b9cbf3579285c6862036d660afe2c287ae5
GabrieldeRivas <gabriel.derivas@estudiantes.unahur.edu.ar>
2023-05-13 12:46:42 -0300

    codigo js

---
+++ main.js
@@ -0,0 +1,17 @@
+let facebookIcon = document.querySelector("#icon-facebook");
+
+facebookIcon.addEventListener("click", () => {
+    location.href ='https://www.facebook.com/Virbela/';
+})
+
+let twitterIcon = document.querySelector("#icon-twitter");
+
+twitterIcon.addEventListener("click", () => {
+    location.href ='https://twitter.com/VirbelaHQ';
+})
+
+let linkedinIcon = document.querySelector("#icon-linkedin");
+
+linkedinIcon.addEventListener("click", () => {
+    location.href ='https://www.linkedin.com/company/virbela/';
+})
